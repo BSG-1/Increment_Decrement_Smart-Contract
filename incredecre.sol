852403d004369a776f4eb9f18d4504d6bd231e98
Ghataorhe <b.ghataorhe@gmail.com>
2018-05-31 23:12:22 -0700

    adds increment & decrement functions that cost gas through changing state of contract

--- incredecre.sol
+++ incredecre.sol
@@ -12,7 +12,17 @@ contract SimpleCounter {
     //view here = reads the state of the data
     //changing state of SC will cost gas (of course); so here we are just trying to read the state of the contract at this point
     function getCounter() view public returns(int){
+        return counter;
+    }
 
+    //no view here bc we are actually changing the state of the coutner -- costs gas 
+    function increment() public {
+        counter += 1;
+    }
+
+    //this will also cost gas as we are once again changing the state of the contract
+    function decrement() public {
+        counter -= 1
     }
 
 }
